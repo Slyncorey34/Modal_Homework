c3ff5d3facb516fec3f27446b240eecb0708aff8
Sarah Corey <syncorey@gmail.com>
2015-12-29 17:35:14 -0500

    Fixed lightbox show/hide

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
-
-// Javascript using jquery library to reveal and hide the modal:
 $(document).ready(
-	function(){
-// when user clicks on the Donate link, show the text modal
-	$(".my-link").on("click",
-	function(e) {
-		e.preventDefault();
-		var $image = $("#p");
-		$image.attr("src",$(this).attr("src") ); 
-	$(".modal").show(1000,function() { 
-	// $("#this-image").attr("src","ab.jpg");
-
-	$(".modal").on("click",
 	function() {
-		$(".modal").hide();
-
-	});
-	});
-});  
-});
 		
+		$('#modal').show(1100,function()  {
+			$('#modal').on('click', function(){
+			$('#modal').hide();
+
+		});
+	
+		});
+	});
